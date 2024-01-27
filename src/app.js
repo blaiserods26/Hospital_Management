c87ba66f983a62a7e325b50b0c156e789cc30d64
Vanessa <vanessarodrigues010506@gmail.com>
2024-01-27 05:59:11 +0530

    server connection established

--- src/app.js
+++ src/app.js
@@ -1,2 +1,64 @@
 const express = require("express");
 const app = express();
+const path = require("path");
+app.set("port", process.env.PORT || 3000); //dynamic port
+const intialPath = path.join(__dirname, "public");
+//app.use(bodyParser.json());
+//app.use(express.static(intialPath));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(intialPath, "index.html"));
+});
+
+app.get("/login", (req, res) => {
+  res.sendFile(path.join(intialPath, "login.html"));
+});
+
+app.get("/register", (req, res) => {
+  res.sendFile(path.join(intialPath, "register.html"));
+});
+
+app.post("/register-user", (req, res) => {
+  const { name, email, password } = req.body;
+
+  if (!name.length || !email.length || !password.length) {
+    res.json("fill all the fields first");
+  } else {
+    db("patients")
+      .insert({
+        name: name,
+        email: email,
+        password: password,
+      })
+      .returning(["name", "email"])
+      .then((data) => {
+        res.json(data[0]);
+      })
+      .catch((err) => {
+        if (err.detail.includes("already exists")) {
+          res.json("email already exists");
+        }
+      });
+  }
+});
+
+app.post("/login-user", (req, res) => {
+  const { email, password } = req.body;
+
+  db.select("name", "email")
+    .from("users")
+    .where({
+      email: email,
+      password: password,
+    })
+    .then((data) => {
+      if (data.length) {
+        res.json(data[0]);
+      } else {
+        res.json("email or password is incorrect");
+      }
+    });
+});
+
+app.listen(3000, (req, res) => {
+  console.log("listening on port 3000......");
+});
