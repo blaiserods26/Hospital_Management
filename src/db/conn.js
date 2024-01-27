0a46ece516b850778611a2809c79cfc961958b0d
Vanessa <vanessarodrigues010506@gmail.com>
2024-01-27 05:30:08 +0530

    mongo connection establish try 1

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,26 +1,19 @@
-
-
 const mongoose = require("mongoose");
-// mongoose.connect("mongodb://localhost:27017");
-const userSchema = new mongoose.Schema(
-    {
-        username :{type : String,
-                  required : true,
-                  unique : true  }, 
+mongoose.connect("mongodb://localhost:27017");
+const userSchema = new mongoose.Schema({
+  username: { type: String, required: true, unique: true },
 
-        email : { 
-            type : String ,
-            required : true ,
-            unique : true 
-        },
-        password : {
-            type: String ,
-            required : true
-        },
-        confirmpassword  :    
-        { 
-            type : String ,
-            required : true 
-        }
-    }
-);
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  confirmPassword: {
+    type: String,
+    required: true,
+  },
+});
