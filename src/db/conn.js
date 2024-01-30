1b25de003e7f51b2b99e54c28389492f9a2912cc
Vanessa <vanessarodrigues010506@gmail.com>
2024-01-30 19:55:21 +0530

    server connection with local host

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,5 +1,23 @@
 const mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost:27017");
+//mongoose.connect("mongodb://localhost:27017");
+
+//const MONGODB_URI = "mongodb://localhost:27017";
+const connectDB = async () => {
+  try {
+    const con = await mongoose.connect("mongodb://localhost:27017/fortis", {
+      //process.env.MONGODB_URI
+      // useNewUrlParser: true,
+      //useUnifiedTopology: true,
+      //useFindAndModify: false,
+      //useCreateIndex: true,
+    });
+    console.log(`MongoDB Database connected with HOST: ${con.connection.host}`);
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
+};
+
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
 
@@ -17,3 +35,4 @@ const userSchema = new mongoose.Schema({
     required: true,
   },
 });
+module.exports = { connectDB };
